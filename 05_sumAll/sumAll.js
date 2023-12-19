3b5b6b214562f48b9387c900e01884ade220570c
Nikil <nik300601@gmail.com>
2023-12-19 16:14:01 +0000

    completed 05

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,28 @@
-const sumAll = function() {
+const sumAll = function(firstNum, secNum) {
+    let result = 0;
+    if (typeof(firstNum) != "number" || typeof(secNum) != "number") {
+        return "ERROR";
+    }
+    else {
+        if (firstNum > -1 && secNum > -1 ) {
+            if (firstNum<secNum) {
+                for (let i = firstNum; i < secNum+1; i++) {
+                    result += i;
+                }
+            }
+            else if (firstNum>secNum) {
+                for (let i = secNum; i < firstNum+1; i++) {
+                    result += i;
+                }
+            }
+        }
+        else {
+            return "ERROR";
+        }
+    }
 
+
+    return result;
 };
 
 // Do not edit below this line

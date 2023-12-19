0cd2f40414c7ec14c3b97b3d0d79db9df4f07fde
Nikil <nik300601@gmail.com>
2023-12-19 13:26:19 +0000

    complete

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,18 @@
-const repeatString = function() {
+
+const repeatString = function(string, number) {
+    let output = "";
+
+    if (number < 0) {
+        return "ERROR";
+
+    }
+    else {
+        for (let i = 0; i < number; i++) {
+            output += string;
+        }
+    }
+    return output;
 
 };
-
 // Do not edit below this line
 module.exports = repeatString;

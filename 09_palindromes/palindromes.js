5c46f93d0ba50f204bf53f235011bd725e413b37
Nikil <nik300601@gmail.com>
2023-12-29 15:51:29 +0000

    09 complete

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,7 @@
-const palindromes = function () {
-
+const palindromes = function (string) {
+    let modString = string.toLowerCase().replace(/[^a-z0-9]/g,'')
+    return modString === modString.split('').reverse().join('');
+//    return modString.split('').reverse().join('')
 };
 
 // Do not edit below this line

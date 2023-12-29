c0c9dda26f8f968b49d7d81a3b9bfab2fc4d794c
Nikil <nik300601@gmail.com>
2023-12-29 15:10:22 +0000

    08 complete

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,37 @@
-const add = function() {
-	
+const add = function(num1, num2) {
+	return num1 + num2;
 };
 
-const subtract = function() {
-	
+const subtract = function(num1, num2) {
+	return num1 - num2;
 };
 
-const sum = function() {
-	
+const sum = function(array) {
+	return array.reduce( (partialSum, currentNum) =>  partialSum + currentNum, 0);
 };
 
-const multiply = function() {
-
+const multiply = function(array) {
+  return array.reduce((partialProduct, a) => partialProduct * a);
 };
 
-const power = function() {
-	
+const power = function(num, power) {
+	return num**power;
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+  let result = num;
+  if (num === 0){
+    return 1;
+  }
+  else {
+    for (let i = 1; i < num; i++) {
+
+      result = result*i;
+    }
+    return result;
+  }
+
+
 };
 
 // Do not edit below this line
